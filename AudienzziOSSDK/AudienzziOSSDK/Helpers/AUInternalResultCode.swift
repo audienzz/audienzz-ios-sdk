ac37866617321409b5f44bf9658cda40352628d7
Yurii Deryzemlia <yurii.deryzemlia@nixs.com>
2024-09-27 12:06:16 +0200

    remove script. resolve some warnings.

--- AudienzziOSSDK/AudienzziOSSDK/Helpers/AUInternalResultCode.swift
+++ AudienzziOSSDK/AudienzziOSSDK/Helpers/AUInternalResultCode.swift
@@ -55,6 +55,8 @@ internal class AUResulrCodeConverter {
             return "SDKMisusePreviousFetchNotCompletedYet".capitalized
         case .prebidInvalidRequest:
             return "InvalidRequest".capitalized
+        @unknown default:
+            fatalError("Unknown Error")
         }
     }
 }
