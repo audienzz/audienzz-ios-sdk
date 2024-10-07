dae05fc07174e1bcbdcd663e6ceef80f7863742a
Yurii Deryzemlia <yurii.deryzemlia@nixs.com>
2024-10-07 16:50:05 +0200

    fix observer issue.

--- AudienzziOSSDK/AudienzziOSSDK/Helpers/VisibleView.swift
+++ AudienzziOSSDK/AudienzziOSSDK/Helpers/VisibleView.swift
@@ -1,11 +1,11 @@
 /*   Copyright 2018-2024 Audienzz.org, Inc.
- 
+
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.
  You may obtain a copy of the License at
- 
+
  http://www.apache.org/licenses/LICENSE-2.0
- 
+
  Unless required by applicable law or agreed to in writing, software
  distributed under the License is distributed on an "AS IS" BASIS,
  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,17 +19,17 @@ import PrebidMobile
 extension UIView {
     func getAllSuperviews() -> [UIView]? {
         var superviews: [UIView] = []
-        
+
         guard let superView = self.superview else {
             return nil
         }
-        
+
         superviews.append(superView)
-        
+
         guard let subViews = superView.getAllSuperviews() else {
             return superviews
         }
-        
+
         superviews.append(contentsOf: subViews)
         return superviews
     }
@@ -37,11 +37,9 @@ extension UIView {
 
 @objcMembers
 public class VisibleView: UIView {
-    
-    public override class func awakeFromNib() {
-        super.awakeFromNib()
-    }
-    
+
+    private var contentOffsetObservations = [NSKeyValueObservation]()
+
     public override func didMoveToWindow() {
         super.didMoveToWindow()
 
@@ -51,53 +49,51 @@ public class VisibleView: UIView {
             removeAsSuperviewObserver()
         }
     }
-    
+
     internal dynamic func detectVisible() {
+        // Implement your visibility detection logic here
     }
-    
+
     public override func removeFromSuperview() {
         removeAsSuperviewObserver()
         super.removeFromSuperview()
     }
-    
+
     deinit {
         removeAsSuperviewObserver()
     }
-    
+
     private func observeSuperviewsOnOffsetChange() {
         guard let superviews = self.getAllSuperviews() else { return }
+
+        // Observe contentOffset of UIScrollView superviews
         for superview in superviews {
-            if superview.responds(to: #selector(getter: UIScrollView.contentOffset)) {
-                superview.addObserver(self, forKeyPath: "contentOffset", options: .new, context: nil)
+            if let scrollView = superview as? UIScrollView {
+                let observation = scrollView.observe(\.contentOffset, options: [.new]) { [weak self] scrollView, _ in
+                    self?.checkIfFrameIsVisible()
+                }
+                contentOffsetObservations.append(observation)
             }
         }
     }
-    
+
     private func removeAsSuperviewObserver() {
-        guard let superviews = self.getAllSuperviews() else { return }
-        for superview in superviews {
-            guard superview.observationInfo != nil else { continue }
-            superview.removeObserver(self, forKeyPath: "contentOffset")
-        }
+        // Invalidate all observations
+        contentOffsetObservations.forEach { $0.invalidate() }
+        contentOffsetObservations.removeAll()
     }
-    
-    public override func observeValue(forKeyPath keyPath: String?, of object: Any?,
-                                      change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
-        if let scroll = object as? UIScrollView, keyPath == "contentOffset", scroll.contentOffset != .zero {
-            checkIfFrameIsVisible()
-        }
-    }
-    
+
     private func checkIfFrameIsVisible() {
         guard let myWindow = self.window else { return }
         let myFrameToWindow = myWindow.convert(self.frame, from: self)
         let myPointToWindow = myWindow.convert(self.frame.origin, from: self.superview)
-        
+
+        // Check if the frame is valid and visible in the window
         if myFrameToWindow.size.width == 0 && myFrameToWindow.size.height == 0 {
             return
         }
-    
-        if CGRectContainsPoint(myWindow.frame, myPointToWindow) {
+
+        if myWindow.frame.contains(myPointToWindow) {
             detectVisible()
         }
     }
