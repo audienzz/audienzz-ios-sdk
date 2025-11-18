340bd42881c986ca2ca61d976768abff463a0e1b
Maksym Ovcharuk <maksym.ovcharuk@nixs.com>
2025-11-18 15:08:42 +0200

    Increased min version of SDK to iOS 15.0

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "AudienzziOSSDK",
-    platforms: [.iOS(.v13)],
+    platforms: [.iOS(.v15)],
     products: [
         .library(
             name: "AudienzziOSSDK",
