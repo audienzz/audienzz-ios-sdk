5a89944dab3a17c403cb4ae46ae729e4e1f6c82f
elysiak <yevhenii.lysiak@audienzz.com>
2026-06-04 12:52:41 +0300

    Release 0.2.2

--- Sources/AudienzziOSSDK/Audienzz.swift
+++ Sources/AudienzziOSSDK/Audienzz.swift
@@ -21,7 +21,7 @@ private let customPrebidServerURL = "https://ib.adnxs.com/openrtb2/prebid"
 private let prebidServerAccountId = "3927"
 private let customStatusEndpoint = "https://ib.adnxs.com/status"
 
-internal let AUSDKVersion = "0.2.1"
+internal let AUSDKVersion = "0.2.2"
 
 @objcMembers
 public class Audienzz: NSObject {
